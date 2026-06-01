a34bd99022ba8a330e2d267f2b8c64582ab82c1b
gama9056 <gama9056@gmail.com>
2026-06-01 17:28:25 -0500

    Update Dashboard_RNTAM.py

--- Dashboard_RNTAM.py
+++ Dashboard_RNTAM.py
@@ -4,6 +4,17 @@ import streamlit as st
 st.set_page_config(page_title="Dashboard RNTambopata", layout="wide", page_icon="🌿")
 
 # ========== DISEÑO DE CUADRÍCULA SIMÉTRICA (ESQUELETO VACÍO MEJORADO) ==========
+/* Caja contenedora del Título Principal */
+    .title-container {
+        border: 2px solid #2c5f2d; /* Verde oscuro natural */
+        border-radius: 12px;
+        padding: 15px 20px;
+        margin-bottom: 25px;
+        background-color: #fefef7; /* Fondo sutilmente claro */
+        box-shadow: 0 4px 10px rgba(0,0,0,0.03);
+        text-align: center;
+    }
+
 # ===========================================================================
 # 1. 📦 CAJA DEL TÍTULO PRINCIPAL (AISLADA EN SU PROPIO CUADRO VERDE)
 # ===========================================================================
